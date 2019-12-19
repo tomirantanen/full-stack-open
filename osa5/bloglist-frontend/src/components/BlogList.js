f2ad1345264ad43d64b412fad428a144097f349e
Tomi Rantanen <tomirantanen@users.noreply.github.com>
2019-12-19 23:48:29 +0200

    5.5

--- osa5/bloglist-frontend/src/components/BlogList.js
+++ osa5/bloglist-frontend/src/components/BlogList.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Blog from "./Blog";
 import BlogForm from "./BlogForm";
+import Togglable from "./Togglable";
 
 const BlogList = ({ user, blogs, handleLogout, handleCreateBlog, notify }) => (
   <>
     <h1>Blogs</h1>
-    <p>{user.name} logged in</p>
-    <button onClick={handleLogout}>logout</button>
-    <BlogForm handleCreateBlog={handleCreateBlog} notify={notify}></BlogForm>
+    <p>
+      {user.name} logged in
+      <button onClick={handleLogout}>logout</button>
+    </p>
+    <Togglable buttonLabel="new blog">
+      <BlogForm handleCreateBlog={handleCreateBlog} notify={notify}></BlogForm>
+    </Togglable>
     {blogs.map(blog => (
       <Blog key={blog.id} blog={blog}></Blog>
     ))}
