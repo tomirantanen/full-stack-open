195f0c546ef6bbfa4893eed6dbaadbd031d9538b
Tomi Rantanen <tomirantanen@users.noreply.github.com>
2019-12-26 22:25:44 +0200

    5.6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,35 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <>
-    <p>
-      {blog.title} {blog.author}
-    </p>
-  </>
-);
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div style={blogStyle} onClick={toggleDetails}>
+      <p>
+        {blog.title} {blog.author}
+      </p>
+      {showDetails ? (
+        <>
+          <p>{blog.url}</p>
+          <p>
+            {blog.likes} likes <button>like</button>
+          </p>
+          <p>Added by {blog.user.username}</p>
+        </>
+      ) : null}
+    </div>
+  );
+};
 
 export default Blog;
