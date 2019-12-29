3224be413e216dc472e1860dab6568014bb4d8bc
Tomi Rantanen <tomirantanen@users.noreply.github.com>
2019-12-29 22:28:02 +0200

    Add error handling to login route

--- osa4/blogilista/controllers/login.js
+++ osa4/blogilista/controllers/login.js
@@ -4,33 +4,37 @@ const loginRouter = require("express").Router();
 const User = require("../models/user");
 const config = require("../utils/config");
 
-loginRouter.post("/", async (request, response) => {
+loginRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
-  const user = await User.findOne({ username: body.username });
-  const passwordCorrect =
-    user === null
-      ? false
-      : await bcrypt.compare(body.password, user.passwordHash);
+  try {
+    const user = await User.findOne({ username: body.username });
+    const passwordCorrect =
+      user === null
+        ? false
+        : await bcrypt.compare(body.password, user.passwordHash);
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: "invalid username or password"
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: "invalid username or password"
+      });
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id
+    };
+
+    const token = jwt.sign(userForToken, config.JWT_SECRET);
+
+    response.status(200).send({
+      token,
+      username: user.username,
+      name: user.name
     });
+  } catch (error) {
+    next(error);
   }
-
-  const userForToken = {
-    username: user.username,
-    id: user._id
-  };
-
-  const token = jwt.sign(userForToken, config.JWT_SECRET);
-
-  response.status(200).send({
-    token,
-    username: user.username,
-    name: user.name
-  });
 });
 
 module.exports = loginRouter;
